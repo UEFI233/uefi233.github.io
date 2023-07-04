c372a976f362492fcc14555626a07d69e61cbd9d
UEFI233 <id_0823911@126.com>
2023-07-04 11:43:28 +0800

    Site updated: 2023-07-04 11:43:28

---
+++ js/my.js
@@ -0,0 +1,7 @@
+// 页面标题start
+window.onscroll = page_title;
+function page_title() {
+    document.getElementById('page-title').style.display = window.location.pathname === '/' || /^\/page\/[0-9]+\//.test(window.location.pathname)
+    document.querySelector('#page-title>span').innerHTML = GLOBAL_CONFIG_SITE.title
+}
+// 页面标题end
